5419434ec99bc246f01ef7905849ba8e2fd0ac89
MerrillAchigan <87760869+MerrillAchigan@users.noreply.github.com>
2024-11-26 23:40:21 +0100

    tests with jasmine framework

---
+++ tests-jasmine/moneyTests.js
@@ -0,0 +1,14 @@
+import {formatCurrency} from '../js/utils/money.js';
+
+describe('test suite : formatCurrency', () => {
+    it('converts cents into dollars', () =>{
+        expect(formatCurrency(2075)).toEqual('20.75');
+    });
+
+    it('works eith 0', () =>{
+        expect(formatCurrency(0)).toEqual('0.00');
+    });
+    it('round up to the nearest up', () =>{
+        expect(formatCurrency(2000.5)).toEqual('20.01');
+    })
+})
