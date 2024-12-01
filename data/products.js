1b49f59db25abbf1b62a26dded199a1f8407e6cb
MerrillAchigan <87760869+MerrillAchigan@users.noreply.github.com>
2024-12-01 18:43:51 +0100

    first use of inheritance with class

--- data/products.js
+++ data/products.js
@@ -34,6 +34,10 @@ class Product{
   getPrice(){
     return `$${formatCurrency(this.priceCents)}`
   }
+
+  extraInfoHTML(){
+    return ``;
+  }
 }
 
 // const product1 = new Product({
@@ -52,6 +56,23 @@ class Product{
 //   ]
 // });
 //console.log(product1);
+
+class Clothing  extends Product {
+  sizeChartLink;
+  
+  constructor(productDetails){
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+  
+  extraInfoHTML(){
+    return `
+    <a href='${this.sizeChartLink}' target='_blank'>
+    Size Charts
+    </a>
+    `
+  }
+}
 
 export const products = [
   new Product({
@@ -713,6 +734,9 @@ export const products = [
     ]
   }
 ].map((productDetails) => {
+  if(productDetails.type === 'clothing'){
+    return new Clothing(productDetails);
+  }
   return new Product(productDetails);
 });
 
