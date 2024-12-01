f79ce6bd1937232d06f27b3809b4da80a0051cdf
MerrillAchigan <87760869+MerrillAchigan@users.noreply.github.com>
2024-12-01 12:11:07 +0100

    localstoragekey n loadfromstorage private

--- data/cart-class.js
+++ data/cart-class.js
@@ -1,18 +1,18 @@
 class Cart {
     cartItems;
 
-    localStorageKey;
+    #localStorageKey;
 
     constructor(localStorageKey) {
-        this.localStorageKey = localStorageKey;
+        this.#localStorageKey = localStorageKey;
        // businessCart.localStorageKey = 'cart-business';
 
-        this.loadFromStorage();
+        this.#loadFromStorage();
         //businessCart.loadFromStorage();
     }
 
-    loadFromStorage(){
-        this.cartItems =  JSON.parse(localStorage.getItem(this.localStorageKey));
+    #loadFromStorage(){
+        this.cartItems =  JSON.parse(localStorage.getItem(this.#localStorageKey));
         if(!this.cartItems){
             this.cartItems = [{
                 productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
@@ -28,7 +28,7 @@ class Cart {
     }
 
     saveToStorage(){
-        localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems))
+        localStorage.setItem(this.#localStorageKey, JSON.stringify(this.cartItems))
     } 
 
     addToCart(productId){
